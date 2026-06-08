f0b56649247dc86ede81f2e6cc8a2b6eb6953fbe
felipefranco-uni9 <felipefranco@uni9.edu.br>
2026-06-08 20:10:52 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,18 +1,15 @@
-// Passo 3: Criar a variável contador iniciando em 0
 let contador = 0;
 
-// Selecionando os elementos do HTML para o JS conseguir manipulá-los
 const elementoNumero = document.getElementById('numero');
 const btnIncrementar = document.getElementById('btnIncrementar');
 const btnDecrementar = document.getElementById('btnDecrementar');
 const btnZerar = document.getElementById('btnZerar');
 
-// Função extra para não repetir código: ela atualiza o texto na tela e checa a cor
+
 function atualizarTela() {
-    // Passo 5: Atualiza o textContent do h1
+   
     elementoNumero.textContent = contador;
 
-    // Passo 6 (Bônus): Muda a cor dependendo do valor
     if (contador > 0) {
         elementoNumero.style.color = "green"; // Positivo fica verde
     } else if (contador < 0) {
@@ -21,8 +18,6 @@ function atualizarTela() {
         elementoNumero.style.color = "black"; // Zero volta para a cor normal
     }
 }
-
-// Passo 4: Adicionando o addEventListener("click") em cada botão
 
 btnIncrementar.addEventListener('click', function() {
     contador++; // Isso é o mesmo que fazer: contador = contador + 1
